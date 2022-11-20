04e975bef003ef50ef3ae731f24b109cbc760d5e
Pedro <pedro.saravia.q@uni.pe>
2022-11-19 21:34:49 -0500

    variables

---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+//* Initial value is "undefined"
+var myName;
+console.log(myName);
+
+//* But it can be assigned a value later
+var myName = "Pedro";
+console.log("My name is " + myName);
+
+//* Or do both in one go;
+var yourName = "Python";
+console.log("Your name is", yourName);
---
+++ python/variables.py
@@ -0,0 +1,17 @@
+
+#! If you don't assign it a value, you'll get an error
+i = 2
+print(i)
+# * Naming convention is important, helps you remember your code
+totalPoints = 100
+
+# * You can assign multiple values
+a, b = 3, 5
+print(a, b)
+
+# * You can delete a variable with the "del" keyword
+the_value = 1
+print(the_value)
+
+del the_value
+print(the_value)
