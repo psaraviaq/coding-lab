ad295f69fa0d8232e9a7e9d915ccb636380b591c
Pedro <pedro.saravia.q@uni.pe>
2022-11-19 20:18:47 -0500

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,13 @@
+// comment
+/*
+also
+a
+comment
+*/
+
+//* Note
+//? What to do
+//! Warning !!!
+//TODO: Do that
+
+console.log("Hello World!"); // inline comment
---
+++ python/first.py
@@ -0,0 +1,11 @@
+# comment
+"""
+a weird comment
+"""
+
+# * This.
+# ? Question
+#! Watch out!
+# TODO: saved for later
+
+print("Hello World!")  # an inline comment
