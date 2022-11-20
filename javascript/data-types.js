8b54a17dda1b71a6e20117da45f8b45f9a650466
Pedro <pedro.saravia.q@uni.pe>
2022-11-20 13:15:17 -0500

    data types in JS

---
+++ javascript/data-types.js
@@ -0,0 +1,28 @@
+//* There are 7 primitive data types in Javascript
+
+//* 1) Number
+console.log(typeof 2);
+
+//* 2) String
+console.log(typeof "Hello?"); // single or double quotes
+console.log(typeof `Tch`);
+
+//* 3) Boolean
+console.log(typeof true);
+console.log(typeof false);
+
+//! 4) Null (using "typeof" will cause a Javascript bug)
+console.log(typeof null);
+
+//* 5) Undefined
+var noValue;
+console.log(noValue);
+
+//* These last two were just introduced in ES6
+//* 6) BigInt
+var bigNumber = BigInt(3);
+console.log(typeof bigNumber);
+
+//? 7) Symbol (I don't know much about this type)
+var sym = Symbol();
+console.log(typeof sym);
