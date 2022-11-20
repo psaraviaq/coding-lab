99c421e61f1221db6a1e55f7865161a1c7d9babc
Pedro <pedro.saravia.q@uni.pe>
2022-11-20 14:40:03 -0500

    data types

---
+++ python/data-types.py
@@ -0,0 +1,22 @@
+
+# * They are classed in literals and consist of
+
+# * 1) Numeric
+print(type(10))  # * Integers
+print(type(-2.7))  # * Floats
+print(type(complex(1, 3)))  # * Complex numbers
+
+# * 2) Sequence
+print(type('hello'))  # * Strings (single or double quotes)
+print(type([0, 1]))  # * Lists
+print(type((2, 3)))  # ? Tuples (values can't change, need more info)
+
+#! 3) Dictionary (keys must be enclosed in quotes)
+print(type({"password": 1234}))
+
+#! 4) Boolean (start with a capital letter)
+print(type(True))
+print(type(False))
+
+# ? 5) Set (values can't be repeated, still need more info)
+print(type({0, 'abc', True}))
