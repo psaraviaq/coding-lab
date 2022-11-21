52dada35227eb3159c691172b325b1056eb4f68b
Pedro <pedro.saravia.q@uni.pe>
2022-11-20 22:57:36 -0500

    operators

---
+++ javascript/operators.js
@@ -0,0 +1,21 @@
+//* Mathematical
+console.log(3 + 4); //* Addition "+"
+console.log(1 - 2); //* Subtraction "-"
+console.log(5 * 6); //* Multiplication "*"
+console.log(20 / 5); //* Division "/"
+console.log(16 % 7); //* Modulus "%"
+console.log(10 ** 2); //* Exponentiation "**"
+
+//* Logical
+console.log(true && false); //* And "&&"
+console.log(false || true); //* Or "||"
+console.log(!true); //* Not "!"
+
+//* Comparison
+console.log(3 < 1);
+console.log(11 >= 11);
+
+console.log(1 == true); //* Equality "=="
+console.log(3 != "3"); //* Inequality "!="
+console.log(0 === "0"); //* Strict equality "==="
+console.log(7 !== 6); //* Strict Inequality "!=="
---
+++ python/operators.py
@@ -0,0 +1,21 @@
+
+# * Mathematical
+print(3 + 4)  # * Addition "+"
+print(1 - 2)  # * Subtraction "-"
+print(5 * 6)  # * Multiplication "*"
+#! Division "/" (returns always a float)
+print(20 / 5)
+print(16 % 7)  # * Modulus "%"
+print(10 ** 2)  # * Exponentiation "**"
+print(74 // 10)  # * Floor division "//"
+
+# *Logical
+print(True and False)  # * "and"
+print(False or True)  # * "or"
+print(not True)  # * "not"
+
+# * Comparison
+print(3 > 1)
+print(11 <= 11)
+print(1 == True)  # * Equal "=="
+print(3 != "3")  # * Not equal "!="
