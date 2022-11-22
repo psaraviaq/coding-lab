06a8dc4241aa3d30af75fd540abb1112f4f8fc3c
Pedro <pedro.saravia.q@uni.pe>
2022-11-21 19:29:22 -0500

    arrays

---
+++ javascript/data-structures/arrays.js
@@ -0,0 +1,19 @@
+//* Examples and methods
+var languages = ["english", "portuguese", "spanish", "french"];
+console.log(languages.join(" "));
+
+//* "push" adds at the end
+languages.push("russian");
+console.log(languages.join(" "));
+
+//* "unshift" adds at the beginning
+languages.unshift("chinese");
+console.log(languages.join(" "));
+
+//* "pop" removes at the end
+console.log(languages.pop());
+console.log(languages.join(" "));
+
+//* "shift" removes at the beginning
+console.log(languages.shift());
+console.log(languages.join(" "));
