e15cbbcfa3deaf40a67142af32e09db74a0a0d40
Pedro <pedro.saravia.q@uni.pe>
2022-11-21 20:49:11 -0500

    sets

---
+++ python/data-structures/sets.py
@@ -0,0 +1,17 @@
+
+# * Examples and methods
+my_set = {1, 2, 3, 4, 3, 3, 3, 5}
+print(my_set)
+
+# * "add" is like the "append" method for lists
+my_set.add(6)
+print(my_set)
+
+# * "remove" (also "discard") must have an argument
+my_set.remove(2)
+print(my_set)
+
+# * There are also another methods like "union", "intersection", .etc
+
+#! Not a "sequence", they don't contain an ordered index of all elements
+print(my_set[1])
