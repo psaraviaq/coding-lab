079ec1cf12cf26896bccbf29757c3323d864fef8
Pedro <pedro.saravia.q@uni.pe>
2022-11-21 21:57:47 -0500

    arguments

--- javascript/functions.js
+++ javascript/functions.js
@@ -4,3 +4,13 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 console.log(factorial(6));
+
+//* "arguments" keyword
+function totalSum() {
+  var sum = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+console.log(totalSum(2, 6, 1, 5));
--- python/functions.py
+++ python/functions.py
@@ -13,3 +13,17 @@ def code():
 print(factorial(5))
 #! if a value doesn't get returned, it outputs "none"
 print(code())
+
+# * "args" keyword
+
+
+def totalSum(*args):
+    sum = 0
+    for x in args:
+        sum += x
+    return sum
+
+
+print(totalSum(10, 3, 8))
+
+# ? There is also "**kwargs"
