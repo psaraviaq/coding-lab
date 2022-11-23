02d7be10b2ecd756e8d66e226e9359f7b2b3ee13
Pedro <pedro.saravia.q@uni.pe>
2022-11-22 23:29:01 -0500

    errors

---
+++ javascript/errors.js
@@ -0,0 +1,13 @@
+//* Useful for handling errors
+try {
+  [2, 3].split();
+} catch (err) {
+  console.log("This is a TypeError");
+}
+
+//! However, it can't handle syntax errors
+try {
+  2+3+
+} catch (err) {
+  console.log("This will never print out")
+}
