dff482961a0b371df9de2901cc0298dcdb54c6d5
Pedro <pedro.saravia.q@uni.pe>
2022-11-23 19:08:37 -0500

    let and const

--- javascript/variables.js
+++ javascript/variables.js
@@ -9,3 +9,24 @@ console.log("My name is " + myName);
 //* Or do both in one go;
 var yourName = "Python";
 console.log("Your name is", yourName);
+
+//* ES6 introduced the block scope
+//* Can assign different values with "let" but not re-declaring it
+let color = "red";
+if (true) {
+  let color = "blue";
+  console.log(color);
+  color = "yellow";
+  console.log(color);
+}
+console.log(color);
+
+//*Can't assign nor re-declare with "const"
+const age = 13;
+if (true) {
+  const age = 17;
+  console.log(age);
+}
+console.log(age);
+//! "const" variables must be assigned a value when declaring them
+const currentJob;
