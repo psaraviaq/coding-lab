e78635f92358096e3dd964392a08e9b1bb390ec2
Pedro <pedro.saravia.q@uni.pe>
2022-11-26 00:47:26 -0500

    sub-classes

--- javascript/classes.js
+++ javascript/classes.js
@@ -14,15 +14,39 @@ class Country {
 
   getTypes() {
     console.log(this);
+
     //* Methods are stored in the prototype of the object
+    const prototype = Object.getPrototypeOf(this);
+
     //! Node.js doesn't display non-enumerable properties (so use O.gOPN())
-    console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(this)));
+    console.log(Object.getOwnPropertyNames(prototype));
+    return prototype;
   }
 }
 
 //* Instance of the class
-const canada = new Country("english", "euro");
+const canada = new Country("english", "canadian dollar");
 
 console.log(`${canada.language}: ${canada.currency}`);
 canada.talk();
 canada.getTypes();
+
+//* Use the "extends" keyword when creating sub-classes
+class EuropeanCountry extends Country {
+  constructor(name, language, currency) {
+    super(language, currency);
+    this.name = name;
+  }
+
+  aboutMe() {
+    const proto = super.getTypes();
+
+    //* Another way to access the prototype
+    console.log(Object.getOwnPropertyNames(proto.__proto__));
+
+    //? The "prototype" property belongs to the constructors.
+  }
+}
+
+const france = new EuropeanCountry("France", "french", "euro");
+france.aboutMe();
