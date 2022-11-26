99181134d5f31e53e23da9befa13a8b75229a5a0
Pedro <pedro.saravia.q@uni.pe>
2022-11-25 21:02:21 -0500

    constructor in python

--- python/classes.py
+++ python/classes.py
@@ -3,15 +3,21 @@
 class Cat:
     sound = "meow"
 
+    # * Similar to the "constructor" in other languages
+    def __init__(self, name, gender) -> None:
+        self.name = name
+        self.gender = gender
+
     # * You can think of "self" as "this" in Javascript
-    def intro(self):
-        print(f'The cat says: {self.sound}')
+    def saySomething(self):
+        print(f'The {self.name} says: {self.sound}')
         self.sound = "purr"
-        print(f'The cat also says: {self.sound}')
+        print(f'The {self.name} also says: {self.sound}')
 
 
 # * Instance of a class
-my_cat = Cat()
+my_cat = Cat("Pacha", "female")
 
+print(my_cat.gender)
 print(my_cat.sound)
-my_cat.intro()
+my_cat.saySomething()
