f7e983d6329a2cea27c296b528f6a0e42e89fa58
Pedro <pedro.saravia.q@uni.pe>
2022-12-05 14:44:54 -0500

    .split default whitespace

--- python/strings.py
+++ python/strings.py
@@ -14,6 +14,9 @@ print("3" > "22")  # * ....
 # * "replace"
 print(sentence.replace("z", "s"))
 
+# * "split"
+print("Yes or No".split())
+
 # * Reversing a string (with slicing)
 print(sentence[::-1])
 
