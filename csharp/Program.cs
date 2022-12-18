d23da4a3719fec5d9c2e26ebe0a5354a5cb27510
Pedro <pedro.saravia.q@uni.pe>
2022-12-17 19:39:55 -0500

    additional info c#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//* "dotnet tool install -g dotnet-script" for Code Runner
+//* "Build Tools for Visual Studio" installed for intellisense
+/*
+  * Configuration (not sure) in 'settings.json'
+
+  "omnisharp.projectLoadTimeout": 10,
+  "omnisharp.useModernNet": true,
+  "omnisharp.path": "",
+  */
+//* Single files don't support intellisense, a project must be created
+
 Console.WriteLine("Hello, World!");
