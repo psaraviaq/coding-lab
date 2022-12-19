79311b667416b11a85b4969bf87f200885ed5a0f
Pedro <pedro.saravia.q@uni.pe>
2022-12-18 21:27:38 -0500

    first php file

---
+++ php/first.php
@@ -0,0 +1,8 @@
+<?php
+
+//* basic example (the php block code starts with "<?php",
+//* or "<?" but this one is not recommended)
+
+echo "Hello, World!";
+
+//! The closing tag ("?"+">") is optional, but not recommended
