05512a38b89431d4c2bc92d9011dbb4118198dd8
Pedro <pedro.saravia.q@uni.pe>
2022-12-20 05:06:27 -0500

    php arrays

---
+++ php/data_structures/arrays.php
@@ -0,0 +1,9 @@
+<?php
+//* for older versions
+$sizes = array("small", "medium", "large");
+echo $sizes[2];
+echo "\n";
+
+//* for recent versions
+$colors = ["red", "blue", "yellow"];
+echo $colors[1];
