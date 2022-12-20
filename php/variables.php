f190d6e476dd7e444ec3ddf77719c318a65c2992
Pedro <pedro.saravia.q@uni.pe>
2022-12-19 23:29:31 -0500

    php variables

---
+++ php/variables.php
@@ -0,0 +1,11 @@
+<?php
+
+//* To declare a variable, you have to use "$"
+$x;
+
+//* Using unassigned variables will output a warning, but still works
+echo $x;
+
+//! ALWAYS use "$" when working with variables
+$x = 7;
+echo $x;
