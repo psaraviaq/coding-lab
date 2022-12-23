504bfd5a75aad89ad059c89950f6a3138821a1f7
Pedro <pedro.saravia.q@uni.pe>
2022-12-22 21:16:48 -0500

    'set' function

--- python/data_structures/sets.py
+++ python/data_structures/sets.py
@@ -11,7 +11,7 @@ print(my_set)
 my_set.remove(2)
 print(my_set)
 
-# * There are also another methods like "union", "intersection", .etc
+# ? There are also other methods like "union", "intersection", .etc
 
 #! Not a "sequence", they don't contain an ordered index of all elements
 # print(my_set[1])
@@ -19,9 +19,12 @@ print(my_set)
 # * Set comprehensions
 print({x for x in range(10) if x not in [1, 3, 4, 5, 6]})
 
-# * There are other ways to work with sets
+# * You can also use other iterable methods with sets
 set2 = set("lists")
-print(set2)
-
 print(sorted(set2))
 print("".join(set2))
+
+# * Using the "set" function may change the order of elements
+print(set([2, 6, 1, 3]))
+# * It's better to do it any other way
+print([*dict.fromkeys([2, 6, 1, 3])])
