996828b6f5b30b625b0dd35d27adcf4a31f1f071
Pedro <pedro.saravia.q@uni.pe>
2023-01-14 03:08:29 -0500

    'export' examples

---
+++ javascript/JS-browser/exporting.js
@@ -0,0 +1,8 @@
+//* Basic use of "export"
+export const number = 17;
+export function greeting() {
+  console.log("Hi World!");
+}
+//! Incorrect use of "export"
+// export color = "red";
+// export myFunction;
