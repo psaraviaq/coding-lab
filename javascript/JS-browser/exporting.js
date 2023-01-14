62b9faaff969d695495152b17aaf74b5e75d76f0
Pedro <pedro.saravia.q@uni.pe>
2023-01-14 03:29:07 -0500

    export default

--- javascript/JS-browser/exporting.js
+++ javascript/JS-browser/exporting.js
@@ -6,3 +6,16 @@ export function greeting() {
 //! Incorrect use of "export"
 // export color = "red";
 // export myFunction;
+
+//* Using the "default" keyword
+export default function catSound() {
+  console.log("Meow");
+}
+// function f1() {}
+//   export default f1;
+// var grade;
+//   export default grade = 10;
+// export default "Starbucks";
+//! Incorrect use of "default"
+// export default const country = "Peru";
+// export default language = "spanish";
