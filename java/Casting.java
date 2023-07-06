c39f2364b1c42c4a64374223595337c1158d6e89
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-06 17:13:44 -0500

    remove casting case

--- java/Casting.java
+++ java/Casting.java
@@ -1,20 +1,12 @@
 public class Casting {
     public static void main(String[] args) {
         numbers();
-        ascii();
     }
 
     static void numbers() {
         //* If you need to use decimals variables in an int, you need to cast them
         double d = 35.25;
         int i = (int) d;
-        System.out.println("The casted int value is " + i);
-    }
-
-    static void ascii() {
-        //* If you need to use a char as an int, you need to cast it
-        char c = 'A';
-        int i = (int) c;
         System.out.println("The casted int value is " + i);
     }
 }
