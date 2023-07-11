891e8ef1e9163a10a1a58e88997ed1b77f3308b5
Pedro Saravia <pedro.saravia.q@uni.pe>
2023-07-11 12:42:10 -0500

    fix print and println

--- java/first.java
+++ java/first.java
@@ -4,13 +4,22 @@
 // * not required when working in the same package
 // * The name must match the filename (compiler rules)
 public class first {
-  
+
     // * "public" is required here
     // * "static": doesn't need to create an instance to run
     // * "void": the method doesn't need to return anything
     // * "String[]" is required, but "args" can be any other name
     public static void main(String[] args) {
+
+        //* "print" doesn't add a new line
+        System.out.print("Oi. ");
+        //* But "println" does, so it's the most common
         System.out.println("Hello, World!");
+
+        //! You can only use one argument when printing
+        // System.out.println("I'm", "Peter");
+        //* But you can concatenate them
+        System.out.println("I'm" + " Peter");
     }
 
 }
