b61cd13fd9076fd32e28abb3c9aad9827b71baaa
Pedro <pedro.saravia.q@uni.pe>
2023-10-26 19:33:33 -0500

    modify basic java files

--- java/First.java
+++ java/First.java
@@ -1,25 +1,19 @@
-// * Basic Example
-
-// * "public": it can be accessed from anywhere,
-// * not required when working in the same package
-// * The name must match the filename (compiler rules)
+//! The class name must be the same as the file name
 public class First {
-
-    // * "public" is required here
-    // * "static": doesn't need to create an instance to run
-    // * "void": the method doesn't need to return anything
-    // * "String[]" is required, but "args" can be any other name
+    
+    //* The "main" method is the entry point of the program, and it must have the signature:
     public static void main(String[] args) {
-
+        
         //* "print" doesn't add a new line
         System.out.print("Oi. ");
+        
         //* But "println" does, so it's the most common
         System.out.println("Hello, World!");
-
+        
         //! You can only use one argument when printing
         // System.out.println("I'm", "Peter");
         //* But you can concatenate them
         System.out.println("I'm" + " Peter");
     }
-
+    
 }
--- java/Loops.java
+++ java/Loops.java
@@ -1,16 +1,17 @@
 public class Loops {
     public static void main(String[] args) {
-        //* Simple for loop (like other languages)
-        for (int i = 1; i < 6; i++) {
+        //* Simple "for" loop
+        for (int i = 1; i < 4; i++) {
             System.out.println(i);
         }
-
-        //* Enhanced for loop (uses an array)
+        
+        //* Enhanced "for" loop (uses an array)
         String[] arr = {"red", "blue", "yellow"};
+        //* First goes the element type, then its variable name, then the array after a colon
         for (String color : arr) {
             System.out.println(color);
         }
-
+        
         //* While loop
         int i = 3;
         while (i > 0) {
