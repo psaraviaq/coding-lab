4d0c6936f7cf4b5f31f82fe74e81bea902881bf6
Pedro <pedro.saravia.q@uni.pe>
2023-11-07 13:33:03 -0500

    modify polymorphism

---
+++ java/oop/Polymorphism.java
@@ -0,0 +1,74 @@
+package oop;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        
+        //* With polymorphism, you can use a subclass object wherever a superclass object is expected.
+        Animal animal = new Dog();
+        //! But the other way around is not possible.
+        // Dog dog = new Animal();
+        
+        //! You can only access the superclass methods.
+        // animal.speak();
+        //* But if the method is also implemented in the subclass, it will call the subclass method.
+        animal.sleep();
+        
+        //* If you want to access subclass methods that are not in the superclass, you must cast the object.
+        Dog dog = (Dog) animal;
+        dog.speak();
+        
+        //* To check the class (or superclass) of the object, you can use the "instanceof" operator.
+        System.out.println(animal instanceof Dog);
+        System.out.println(animal instanceof Animal);
+        
+        getAnimal();
+        feedAnimal(new Dog());
+    }
+    
+    //* You can return a subclass object from a method that expects a superclass.
+    public static Animal getAnimal() {
+        return new Dog();
+    }
+    
+    //* And you can pass a subclass object to a method that expects a superclass.
+    public static void feedAnimal(Animal animal) {
+        System.out.println("Feeding animal...");
+    }
+}
+
+class Animal {
+    void sleep() {
+        System.out.println("Sleeping...");
+    }
+    
+    Animal eat() {
+        System.out.println("Eating...");
+        return new Animal();
+    }
+    
+    void print(Animal animal) {
+        System.out.println("Animal");
+    }
+}
+
+class Dog extends Animal {
+    void sleep() {
+        System.out.println("Sleeping like a dog...");
+    }
+    
+    void speak() {
+        System.out.println("Woof!");
+    }
+    
+    //* When overriding a method, arguments must be the SAME, and return types must be COMPATIBLE.
+    //* That means the return type can be a subclass, and the argument names can be different.
+    Dog eat() {
+        System.out.println("Eating like a dog...");
+        return new Dog();
+    }
+    
+    //! But making the argument type a subclass is not overriding, it's overloading.
+    void print(Dog dog) {
+        System.out.println("Dog");
+    }
+}
