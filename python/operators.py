004e9c3b2496e42f4da9ccd53b6fbdf2a8015d2e
Pedro <pedro.saravia.q@uni.pe>
2023-11-10 21:59:05 -0500

    modify operators

--- python/operators.py
+++ python/operators.py
@@ -1,48 +1,32 @@
 
-# * Mathematical
-print(3 + 4)  # * Addition "+"
-print(1 - 2)  # * Subtraction "-"
-print(5 * 6)  # * Multiplication "*"
-#! Division "/" (returns always a float)
-print(20 / 5)
-print(16 % 7)  # * Modulus "%"
-print(10 ** 2)  # * Exponentiation "**"
-print(74 // 10)  # * Floor division "//"
+#* ----------------- Mathematical -----------------
+3 + 4    #* Addition "+"
+1 - 2    #* Subtraction "-"
+5 * 6    #* Multiplication "*"
+20 / 5   #! Division "/" (returns always a float)
+16 % 7   #* Modulus "%"
+10 ** 2  #* Exponentiation "**"
+74 // 10 #* Floor division "//"
 #! Python doesn't support "++" and "--"
-print("_________________________________________")
+# i++
 
-# *Logical
-print(True and False)  # * "and"
-print(False or True)  # * "or"
-print(not True)  # * "not"
-print("_________________________________________")
+#* ----------------- Logical -----------------
+True and False   #* "&&"
+False or True    #* "||"
+not True         #* "!"
 
-# * Comparison
-print(3 > 1)
-print(11 <= 11)
-print(1 == True)  # * Equal "=="
-print(3 != "3")  # * Not equal "!="
-print("_________________________________________")
+#* ----------------- Comparison -----------------
+3 > 1
+11 <= 11
+1 == True  # * Equal "=="
+3 != "3"  # * Not equal "!="
 
-# * Others
-print(3 in [1, 3, 6])  # * "in" / "not in"
+#* ----------------- Others -----------------
+#* "in" / "not in" check if a value is in a sequence
+print(3 in [1, 3, 6])
 print("us" in "sushi")
 print(2 not in (2, 7, 4))
-print("_________________________________________")
-
-#! Python built-in methods are lowercase instead of camelCase
+#? Other operators: is, of, with, yield
 
 #! Do not use the "eval" function, can lead to running malicious code
 print(eval("1+2"))
-
-# * Built-in functions that are not included in the "math" library
-print(abs(-7))                # * "abs"
-print(max([2, 9, 15, 7]))     # * "max"
-print(min(2, 9, 15, 7))       # * "min"
-print(f'min: {min(["a", "b", "c"])}, max: {max("xyz")}')
-#! "round" doesn't work like in other languages
-print(round(4.5))
-# * but it can be used for precision too with a second argument
-print(round(15/7, 3))
-
-# ? Other operators: is, of, in, with, yield
