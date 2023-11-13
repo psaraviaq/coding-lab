d7d979bdcc7511b4922aaaa244527aec28c2943a
Pedro <pedro.saravia.q@uni.pe>
2023-11-13 15:55:48 -0500

    add strings and arrays

---
+++ kotlin/kotlin/src/main/kotlin/Strings.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name = "Pedro"
+
+    //* You can access the characters of a string using square brackets
+    println(name[0])
+
+    //* "length" works as a property, not a method like in Java
+    println(name.length)
+
+    //* Using "==" makes a call to "equals" method
+    println(name == "Pedro")
+    //! To compare references, use "==="
+}
---
+++ kotlin/kotlin/src/main/kotlin/datastructures/KotlinArrays.kt
@@ -0,0 +1,8 @@
+package datastructures
+
+fun main() {
+    var numbers = arrayOf(1, 2, 3)
+
+    //* Use the "size" property to get the size of the array
+    println(numbers.size)
+}
