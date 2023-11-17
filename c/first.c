f47260a1a59c997e5a486d2b59170a598888f764
Pedro <pedro.saravia.q@uni.pe>
2023-11-17 15:50:36 -0500

    add first C program

---
+++ c/first.c
@@ -0,0 +1,8 @@
+//* This is needed for the "printf" function and its relatives
+#include <stdio.h>
+
+int main(void)
+{
+    //! "printf" does not add a newline character by default, so you have to add it yourself
+    printf("hello, world\n");
+}
