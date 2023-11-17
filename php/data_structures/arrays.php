06452b019fdc0ff6a595fb5376e30ac8e0b8ab58
Pedro <pedro.saravia.q@uni.pe>
2023-11-16 19:11:21 -0500

    delete kotlin and php

--- php/data_structures/arrays.php
+++
@@ -1,9 +0,0 @@
-<?php
-//* for older versions
-$sizes = array("small", "medium", "large");
-echo $sizes[2];
-echo "\n";
-
-//* for recent versions
-$colors = ["red", "blue", "yellow"];
-echo $colors[1];
--- php/first.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//* basic example (the php block code starts with "<?php",
-//* or "<?" but this one is not recommended)
-
-echo "Hello, World!";
-
-//! The closing tag ("?"+">") is optional, but not recommended
