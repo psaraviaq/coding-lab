9be2174ad498118b75e576f940cfb24a3f65fe2b
Pedro <pedro.saravia.q@uni.pe>
2023-12-26 19:30:43 -0500

    add lambdas with more than one line

--- java/miscellaneous/Lambdas.java
+++ java/miscellaneous/Lambdas.java
@@ -2,8 +2,10 @@ package miscellaneous;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("unused")
 public class Lambdas {
     
     public static void main(String[] args) {
@@ -17,6 +19,12 @@ public class Lambdas {
         vowels.forEach(consumer);
         //* "forEach" is a minified version of a for loop.
         // vowels.forEach(s -> System.out.println(s));
+
+        //* Lambdas can have more than one line of code
+        Function<String, String> toUpperCase = s -> {
+            System.out.println("Converting to uppercase...");
+            return s.toUpperCase();
+        };
 
         //* Method references are a shorthand for lambda expressions when they're just a method call
         //* First, you put the class name, then instead of ".", you put "::", and then the method name
