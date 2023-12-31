459924b6177e9bc5e9c087be4c034c70c0ef2bdc
Pedro <pedro.saravia.q@uni.pe>
2023-12-31 16:36:19 -0500

    add more about types

--- c/data-types.c
+++ c/data-types.c
@@ -4,11 +4,20 @@
 
 int main(void)
 {
+    //* Declaration
+    // int age;
+    //* Assignment
+    // age = 23;
+    //* Initialization
+    // int age = 23;
+
     //* Integer
     int age = 23;
 
     //* Character
     char letter = 'A';
+    //! You can treat characters as integers and vice-versa
+    printf("%i\n", letter);
 
     //* The "string" type is not a primitive type in C, but a library from CS50
     string name = "Pedro";
@@ -37,9 +46,11 @@ int main(void)
     const int n = 5;
     // n = 6;
 
-    // string sentence = get_string("What's your name?: ");
-    // printf("Hi %s!\n", sentence);
-
     //* You could also declare multiple variables at once
     int a = 1, b = 2, c = 3;
+
+    for (int i = 0, j = 3; i < j; i++)
+    {
+        printf("%i\n", i);
+    }
 }
