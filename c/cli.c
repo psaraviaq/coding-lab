1fa1d53ea18823d3278763af6b9f7384588e880b
Pedro <pedro.saravia.q@uni.pe>
2024-01-04 11:33:39 -0500

    add elements of arguments

--- c/cli.c
+++ c/cli.c
@@ -3,13 +3,16 @@
 //* int main(int argc, string argv[])
 
 //* When you run a program, you can pass arguments to it
-//* "argc" is the number of arguments passed, "argv" is an array of strings containing the arguments
+//* "argc" is the number of arguments passed, "argv" is always an array of strings containing the arguments
 //! There's always at least one argument, the name of the program itself
 int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
         printf("Hello, %s\n", argv[1]);
+
+        //* You can also access individual characters of an argument
+        printf("The first letter of your name is %c\n", argv[1][0]);
     }
     else
     {
