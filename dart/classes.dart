6dbbd6b4c42c16e44c8cad15f557331dc140b219
pedro <pedro.saravia.q@uni.pe>
2024-04-15 22:38:29 -0500

    add classes

--- dart/classes.dart
+++ dart/classes.dart
@@ -1,15 +1,28 @@
 void main() {
-  //* You don't need to use "new" to create an object
-  User user = User("John");
+  //* You don't need the "new" keyword to create an object
+  var user = User("John", 21);
 
-  print(user.name);
+  //* When printing the properties of an object in a string, use "{}" to access them
+  print("${user.name} is ${user.age} years old");
+
+  var worker = Worker("Developer", "Jane", 25);
+  print("${worker.name} - ${worker.age} years old - ${worker.job}");
 }
 
 class User {
-  //! If the variable can be null, you need to add a question mark
-  String? name;
+  String name;
+  int age;
 
-  User(String name) {
-    this.name = name;
-  }
+  //* You can directly assign the properties in the constructor by using "this." in the parameters
+  //! Add a semicolon after the constructor in this case
+  User(this.name, this.age);
+}
+
+class Worker extends User {
+  String job;
+
+  //* You can also assign the properties to the parent class by using "super" after the constructor
+  // Worker(this.job, String name, int age) : super(name, age);
+  //* But a more efficient way is to directly use "super." in the parameters
+  Worker(this.job, super.name, super.age);
 }
