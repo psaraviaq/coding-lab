fa4231008510e29ff907e7e69f853699a2be4d84
pedro <pedro.saravia.q@uni.pe>
2024-04-15 21:10:45 -0500

    fix comments 110

--- java/miscellaneous/Exceptions.java
+++ java/miscellaneous/Exceptions.java
@@ -5,52 +5,54 @@ import javax.sound.midi.Sequencer;
 @SuppressWarnings("unused")
 public class Exceptions {
 
-    //* Exceptions are objects that represent an error or unexpected situation
+    //* Exceptions are objects that represent an error or an unexpected situation
     //* They are thrown by methods that can't guarantee success
 
     public static void main(String[] args) {
         Exceptions midi = new Exceptions();
         System.out.println(midi.play());
 
-        //* this don't need try/catch because runtimeException
+        //* This doesn't need a "try/catch" because is a runtime exception
         // Integer.parseInt("null");
     }
 
-    //* if call method that might throw (NOT or directly throw yourself NOT) (throw a not runtimeException). u must acknowledge, one way is wrap it with try/catch
-    //* if called method throw a runtime, then try/catch not needed
     public int play() {
-        //* try/catch for unexpected situations (that can't guarantee success), not for flaws in code
-        //* You can optionally use "finally" to run code after try/catch, even if it returns something
-        //* You can have multiple catch blocks for different exceptions
-        //! Exceptions are polymorphic, so you can catch a superclass for its subclasses
-        //! But you can't catch a superclass and then a subclass, because it will never reach the subclass, and it won't compile either
+        //* Use a "try/catch" block to encapsulate and deal with exceptions
+        //! But not for flaws in your code that you can fix, only for things you can't control
+        //* All exceptions must be caught, except for runtime exceptions which are optional
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             System.out.println("Success");
             // throw new Exception();
             return 0;
+            //* You can have multiple "catch" blocks for different types of exceptions
+            //! Exceptions are polymorphic too, so you can declare a superclass and catch subclasses
+            //! But you can't declare a superclass, and then a subclass, because the subclass will never be reached, and it will give a compilation error
         } catch (Exception e) {
             System.out.println(":(");
-            //* "printStackTrace" prints the exception and where it happened
+            //* "printStackTrace" prints the exception and also where it happened
             e.printStackTrace();
             return 1;
+        //* You can optionally use "finally" to run code after the block, even if it already returned something
         } finally {
             System.out.println("!");
-            //! However, if you return in finally, it will override the return in either try or catch
+            //! However, if you return something here, it will override the return value in "try/catch"
             // return 2;
         }
     }
 
-    //* if method might throw exception, declare in the method, use "throws" keyword with the exception class (or more separated by comma)
-    //* also you can declare superclass and throw subclasses
+    //! If a method might throw an exception, you must declare it in the method signature
+    //* Use the "throws" keyword with the exception class (use commas to add more exceptions)
+    //* You can also declare a superclass and throw subclasses
     public void myMethod() throws Exception {
         if (true) {
-            //? you don't have to declare runtime either, but good practice?
-            // throw new NullPointerException();
-            //* exceptions too objects, use "throw" keyword and new exception object
+            //* Here you use the "throw" keyword to throw an exception
+            //! Because exceptions are objects too, you have to create them with "new"
             throw new Exception();
+            //! It is pointless to throw a runtime exception, since they mean bad code
+            // throw new NullPointerException();
         }
     }
 
-    //* you can also duck exception by declaring again the exception thrown by method (even main)
+    //* You can duck exceptions by declaring again the exception in the method that calls the method that throws
 }
