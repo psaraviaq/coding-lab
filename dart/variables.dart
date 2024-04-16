06f8b7e5c8da287b3c47d8f7c9fc4a13e86b95e1
pedro <pedro.saravia.q@uni.pe>
2024-04-15 22:37:13 -0500

    strings with single quotes

--- dart/variables.dart
+++ dart/variables.dart
@@ -15,12 +15,14 @@ void main() {
   print("My name is $name and I am $age years old.");
 
   //* You can also specify the type of the variable
-  // String city = "New York";
   // int population = 8_623_000;
   // bool isCapital = false;
   // double area = 468.9;
+  // String city = "New York";
+  //! Strings can also be declared using single quotes
+  // String city = 'Austin';
 
   //! To use variables without initializing them, add the "?" operator
-  String? city;
-  print(city);
+  String? course;
+  print(course);
 }
