01ec4d47ddd929dacf2c360d7fd7afe865ac747d
pedro <pedro.saravia.q@uni.pe>
2024-04-17 11:29:04 -0500

    add object serialization

--- java/miscellaneous/Serialization.java
+++ java/miscellaneous/Serialization.java
@@ -1,30 +1,47 @@
 package miscellaneous;
 
+//* Import the "java.io" package to perform I/O operations
 import java.io.*;
 
 public class Serialization {
     public static void main(String[] args) {
-        Product product = new Product();
-        product.price = 20;
+        
+        Product product = new Product(20);
+
+        //! I/O operations are prone to exceptions, so we need to handle them
+        try {
+            //* To connect to a file, we need to create a "FileOutputStream" object, pass the filename as an argument
+            FileOutputStream fs = new FileOutputStream("Products.ser");
+
+            //* To write objects to the file, create an "ObjectOutputStream" object, and pass the previous object
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+
+            //* You can then write objects to the last stream with the "writeObject" method
+            os.writeObject(product);
+
+            //* At the end, close the stream with the "close" method
+            os.close();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         try {
-            FileOutputStream fOutputStream = new FileOutputStream("MyProduct.ser");
-            ObjectOutputStream oOutputStream = new ObjectOutputStream(fOutputStream);
-            oOutputStream.writeObject(product);
-            oOutputStream.close();
-
-            FileInputStream fInputStream = new FileInputStream("MyProduct.ser");
-            ObjectInputStream oInputStream = new ObjectInputStream(fInputStream);
-            Product item = (Product) oInputStream.readObject();
-            oInputStream.close();
+            FileInputStream fs = new FileInputStream("Products.ser");
+            ObjectInputStream os = new ObjectInputStream(fs);
+            Product item = (Product) os.readObject();
+            os.close();
 
             System.out.println("Price: " + item.price);
         } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
 
 class Product implements Serializable {
     int price;
+
+    public Product(int price) {
+        this.price = price;
+    }
 }
