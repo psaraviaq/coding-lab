4cb3eeef60822c5d1509993f9f1fafb97e89d92b
pedro <pedro.saravia.q@uni.pe>
2024-04-25 20:11:06 -0500

    add object deserialization

--- java/miscellaneous/Serialization.java
+++ java/miscellaneous/Serialization.java
@@ -9,6 +9,8 @@ public class Serialization {
         Product product = new Product("Laptop", 1000);
 
         //! I/O operations are prone to exceptions, so we need to handle them
+
+        //* How to serialize objects
         try {
             //* To connect to a file, we need to create a "FileOutputStream" object, pass the filename as an argument
             //* It will create a file with the given name if it doesn't exist
@@ -24,17 +26,34 @@ public class Serialization {
             os.close();
             
         } catch (Exception e) {
-            e.printStackTrace();
         }
 
+        //* How to deserialize objects
         try {
+            //* To connect to a file, create a "FileInputStream" object, and pass the filename as an argument
             FileInputStream fs = new FileInputStream("Products.ser");
+
+            //* To read objects from the file, create an "ObjectInputStream" object, and pass the previous object
             ObjectInputStream os = new ObjectInputStream(fs);
-            Product item = (Product) os.readObject();
+            
+            //* You can then read objects from the last stream with the "readObject" method
+            //! The method always return an "Object", so you must cast it later if you want to use it
+            // Object object = os.readObject();
+            // Product item = (Product) object;
+            // * Or you can just cast it directly when reading the object
+            Product laptop = (Product) os.readObject();
+
+            //* You can then use the object as you would normally
+            System.out.println("Price: " + laptop.price);
+
+            //! But if you try to read more objects than the file has, you'll get a runtime error
+            // Product _laptop = (Product) os.readObject();
+            
+            //* You can use "close" again to close the stream
             os.close();
 
-            System.out.println("Price: " + item.price);
         } catch (Exception e) {
+            System.out.println("No more objects to read");
         }
     }
 }
