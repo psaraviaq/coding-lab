049f5291c4f9b81af5f2e720030d7712323aa537
pedro <pedro.saravia.q@uni.pe>
2024-05-07 16:45:41 -0500

    add reading from a connection

--- java/miscellaneous/Channels_Client.java
+++ java/miscellaneous/Channels_Client.java
@@ -9,16 +9,25 @@ import java.nio.charset.StandardCharsets;
 
 public class Channels_Client {
     public static void main(String[] args) {
-        InetSocketAddress serverAddress = new InetSocketAddress("localhost", 5000);
-
         try {
+            //! The "InetSocketAddress" creates an address object.
+            //* We pass the IP address (or even "localhost") and the port number of the server we want to connect to.
+            InetSocketAddress serverAddress = new InetSocketAddress("localhost", 5000);
+        
+            //! The "SocketChannel" object represents a network connection between two machines.
+            //* Pass the address object to the "open" method to create the connection object.
             SocketChannel socketChannel = SocketChannel.open(serverAddress);
 
-            Reader r = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+            //! To communicate with the server, we can use the "Channels" class.
+            //* The "newReader" method creates a "Reader" object that reads from the server.
+            //* Pass the connection object and the "UTF_8" charset as arguments.
+            Reader reader = Channels.newReader(socketChannel, StandardCharsets.UTF_8);
+
+            //* To make the reading easier, wrap it in a "BufferedReader".
+            BufferedReader br = new BufferedReader(reader);
             
-            BufferedReader reader = new BufferedReader(r);
-            
-            String line = reader.readLine();
+            //* And you can use the same methods as with a file to read the data.
+            String line = br.readLine();
 
             System.out.println(line);
         } catch (Exception e) {
