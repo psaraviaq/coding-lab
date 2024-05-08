565c9c87167131375974753c3b5cd137fe64049c
pedro <pedro.saravia.q@uni.pe>
2024-05-07 21:19:38 -0500

    fix comments 118

--- java/miscellaneous/Channels_Server.java
+++ java/miscellaneous/Channels_Server.java
@@ -10,20 +10,37 @@ public class Channels_Server {
     public static void main(String[] args) {
         try {
             ServerSocketChannel serverChannel = ServerSocketChannel.open();
+
+            //! The "bind" method starts the server on the specified address object.
+            //* The address uses the port number as the only argument.
             serverChannel.bind(new InetSocketAddress(5000));
 
             while (serverChannel.isOpen()) {
                 System.out.println("Waiting...");
-                SocketChannel clientChannel = serverChannel.accept();
 
-                Writer w = Channels.newWriter(clientChannel, StandardCharsets.UTF_8);
-                PrintWriter writer = new PrintWriter(w);
-                
-                writer.println("Hello!");
+                //* Create the connection object when a client connects.
+                SocketChannel socketChannel = serverChannel.accept();
 
-                writer.close();
+                System.out.println("Connected!");
+
+                //* We have to use the "Channels" class again to communicate.
+                //! But now we use the "newWriter" method to create a "Writer" object.
+                //* The charsets for reading and writing should be the same.
+                Writer writer = Channels.newWriter(socketChannel, StandardCharsets.UTF_8);
+
+                //* We could wrap it in a "BufferedWriter"
+                // BufferedWriter bw = new BufferedWriter(writer);
+                //! But it is more common to use the "PrintWriter" class.
+                PrintWriter pw = new PrintWriter(writer);
+
+                //! And now you use other methods like "print" and "println".
+                pw.println("Hello");
+
+                //* Don't forget to close it again.
+                pw.close();
             }
         } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
