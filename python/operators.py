19a4eb9b050a82077bb67271184b1f6d82543fc6
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-09 12:40:19 -0500

    add operators

--- python/operators.py
+++ python/operators.py
@@ -1,32 +1,26 @@
 
 #* ----------------- Mathematical -----------------
-3 + 4    #* Addition "+"
-1 - 2    #* Subtraction "-"
-5 * 6    #* Multiplication "*"
-20 / 5   #! Division "/" (returns always a float)
-16 % 7   #* Modulus "%"
-10 ** 2  #* Exponentiation "**"
-74 // 10 #* Floor division "//"
+#* Addition "+"
+#* Subtraction "-"
+#* Multiplication "*"
+#! Division "/" (returns always a float)
+#* Modulus "%"
+#* Exponentiation "**"
+#* Floor division "//"
 #! Python doesn't support "++" and "--"
 # i++
 
 #* ----------------- Logical -----------------
-True and False   #* "&&"
-False or True    #* "||"
-not True         #* "!"
-
-#* ----------------- Comparison -----------------
-3 > 1
-11 <= 11
-1 == True  # * Equal "=="
-3 != "3"  # * Not equal "!="
+#* and ("&&")
+#* or ("||")
+#* not ("!")
 
 #* ----------------- Others -----------------
-#* "in" / "not in" check if a value is in a sequence
+
+#* "in" / "not in"
 print(3 in [1, 3, 6])
 print("us" in "sushi")
 print(2 not in (2, 7, 4))
-#? Other operators: is, of, with, yield
 
-#! Do not use the "eval" function, can lead to running malicious code
-print(eval("1+2"))
+#! Don't use "eval" for production code
+# print(eval("1+2"))
