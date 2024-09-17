685e26f0cb80beb55e39ea195773074ece2cd3c8
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-16 19:33:45 -0500

    add sets

--- python/data_structures/sets.py
+++ python/data_structures/sets.py
@@ -1,16 +1,14 @@
-my_set = {3, 2, 3, 7}
-print(my_set)
+nums = {7, 2, 3, 7, 2}
+print(f'nums={{7, 2, 3, 7, 2}}:\t{nums}')
 
-#* Sets have the "add" and "remove" methods
-my_set.add(6)
-print(my_set)
+#* "add", "remove"
+nums.add(11)
+print(f'\n.add(11):\t\t{nums}')
+nums.remove(2)
+print(f'.remove(2):\t\t{nums}')
 
-my_set.remove(2)
-print(my_set)
-
-#* The "set" function can be used to create a set from a sequence
-set2 = set("lists")
-print(set2)
+#* set()
+print(f'\nset("lists"):\t\t{set("lists")}')
 
 #* Set comprehensions
 # print({x for x in range(10) if x not in [1, 3, 4, 5, 6]})
