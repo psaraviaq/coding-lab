b050555aa6ef83000d83374c98d3581577907389
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-16 19:08:50 -0500

    add in for dicts

--- python/operators.py
+++ python/operators.py
@@ -21,6 +21,7 @@
 print(3 in [1, 3, 6])
 print("us" in "sushi")
 print(2 not in (2, 7, 4))
+print("id" in {"id": 3}) #* Same as .keys()
 
 #! Don't use "eval" for production code
 # print(eval("1+2"))
