d0a3e742340074c72457dd55fde2e0aa244f8b17
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-16 19:19:53 -0500

    add dictionaries

--- python/data_structures/dictionaries.py
+++ python/data_structures/dictionaries.py
@@ -1,39 +1,29 @@
 
-#* A "dictionary" is like an object in other languages
-#! Keys are required to be in quotes
-person = {"name": "Pedro", "age": 22}
-print(person)
+person = {"name": "Pedro", "age": 22, "color": "blue"}
+print(f"person:\t\t\t\t{person}")
 
-#* You can access the value associated with a key using brackets
-print(person["name"])
-#! But if the key doesn't exist, it will throw an error
-# print(person["country"])
-#! In Python, you can't use the dot notation to access the value
+#*-------------- Access --------------
+print(f'\n["name"]: {person["name"]}')
+#! You can't use the dot notation
 # print(person.name)
+#* "get" is a safer way to access
+print(f'.get("degree"): {person.get("degree")}')
+# print(person.get("country", "Peru"))
 
-#* To avoid errors, you can use the "get" method, which returns "None" if the key doesn't exist
-print(person.get("degree"))
-#* You can even add a default value (second argument) to return if the key doesn't exist
-print(person.get("country", "USA"))
-
-#* To add a new key-value pair, or update an existing one, you can use brackets
-person["color"] = "blue"
-person["age"] = 23
-print(person)
-
-#* You can also use "del" to delete a key-value pair
+#* "del" removes a key-value pair
 del person["color"]
-print(person)
+print(f'\ndel person["color"]:\t\t{person}')
 
-#* You can use "items()" to get a list of tuples with the key-value pairs
+#*-------------- Loops ---------------
+print("\n.items(): key, value")
 for key, value in person.items():
-    print(f'(Pair) {key}: {value}')
+    print(f'\t{key}:\t{value}')
 
-#* Or you can just use "keys()" and "values()" to get them separately
-for key in person.keys():
-    print("key: " + key)
-for value in person.values():
-    print(f'value: {value}')
+print("\n.keys()\t.values()")
+for key, value in person.keys(), person.values():
+    print(f'{key}\t{value}')
+
+#*-------------- Others ---------------
 
 #* Dictionary comprehensions
 # print({str(x): x**3 for x in range(1, 6)})
