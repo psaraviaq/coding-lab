c4bde8f86d5c26c697528377273bcf482420403c
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-09-19 04:56:11 -0500

    add functions

--- python/functions.py
+++ python/functions.py
@@ -1,16 +1,25 @@
 
-#* Functions use the keyword "def"
-#* When you don't return anything, it returns "None"
+#* "def"
 def double(x):
-
-    #* Docstrings add documentation to functions
-    """This function multiplies a number by 2"""
-
     return x * 2
 
 print(double(5))
-#! Parameter values are required unless you set a default value
+#! Arguments are required
 # print(double())
+
+
+#* "None"
+def no_return():
+    pass
+
+print(no_return())
+
+
+
+
+
+
+
 
 #* You can pass an arbitrary number of arguments using a "*" before the parameter
 #* It converts the arguments into a tuple (iterable)
@@ -25,7 +34,3 @@ def info(**person):
     print(person)
 
 info(name="John", age=22, height=1.80)
-
-#* If you want to have a function that doesn't do anything yet, use the "pass" keyword
-def test_code():
-    pass
