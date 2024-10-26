22e4f33d375bb8a099c5c93917a5b37b766f0d63
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-10-25 20:29:46 -0500

    add optional properties

--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -1,8 +1,10 @@
 //* An interface defines the structure of an object in terms of properties and methods
 //* It is a way to define a contract in your code and ensure that the objects you use have the same properties and methods.
+//! Use "?" to make a property optional
 interface Person {
   name: string;
   age: number;
+  address?: string;
   greet: () => void;
   // greet(): void;
 }
