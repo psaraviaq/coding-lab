fef447c65341ae2edf093845a210c566836ecc1b
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-10-25 20:46:00 -0500

    add variables

---
+++ typescript/variables.ts
@@ -0,0 +1,9 @@
+//* If you don't specify a type, TypeScript will infer it from the value you assign to the variable.
+let number = 5;
+//! So you can't assign a different type to the variable
+// number = "2";
+
+//* For arrays, you can either use the type followed by []
+let fruits: string[];
+//* Or you can use the Array type
+// let fruits: Array<string>;
