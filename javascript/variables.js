0f213f6475aa1aad5b28ee80de5bb4fea500e232
Pedro <pedro.saravia.q@uni.pe>
2024-12-21 21:12:17 -0500

    refactor variables

--- javascript/variables.js
+++ javascript/variables.js
@@ -1,18 +1,20 @@
-//* Initial value is "undefined"
-var myName;
-console.log(myName);
+// // Initial value is "undefined"
+// var myName;
+// console.log(myName);
 
-//* But it can be assigned a value later
-var myName = "Pedro";
-console.log("My name is " + myName);
+// // But it can be assigned a value later
+// var myName = "Pedro";
+// console.log("My name is " + myName);
 
-//* Or do both in one go;
-var yourName = "Python";
-console.log("Your name is", yourName);
+// // Or do both in one go;
+// var yourName = "Python";
+// console.log("Your name is", yourName);
 
-//* ES6 introduced the block scope
-//* Can assign different values with "let" but not re-declaring it
+//~ ES6 introduced the block scope
+
+//* With "let" you can reassign values with but not redeclare the variable.
 let color = "red";
+// let color;
 if (true) {
   let color = "blue";
   console.log(color);
@@ -20,12 +22,16 @@ if (true) {
   console.log(color);
 }
 console.log(color);
+//?...
+let person;
+console.log(person);
 
-//*Can't assign nor re-declare with "const"
+//* With "const" you can't reassign nor redeclare.
 const age = 13;
 if (true) {
   const age = 17;
   console.log(age);
+  // age = 20;
 }
 console.log(age);
 //! "const" variables must be assigned a value when declaring them
