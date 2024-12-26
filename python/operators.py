a4fa88888abdc749096db7096950772c8e967386
Pedro <pedro.saravia.q@uni.pe>
2024-12-26 17:22:58 -0500

    add multiple assignment

--- python/operators.py
+++ python/operators.py
@@ -1,27 +1,31 @@
+#
+# * Multiple assignment
+a, b, c = 1, 2, 3
+print(a, b, c)
 
-#* ----------------- Mathematical -----------------
-#* Addition "+"
-#* Subtraction "-"
-#* Multiplication "*"
-#! Division "/" (returns always a float)
-#* Modulus "%"
-#* Exponentiation "**"
-#* Floor division "//"
-#! Python doesn't support "++" and "--"
+# * ----------------- Mathematical -----------------
+# * Addition "+"
+# * Subtraction "-"
+# * Multiplication "*"
+# ! Division "/" (returns always a float)
+# * Modulus "%"
+# * Exponentiation "**"
+# * Floor division "//"
+# ! Python doesn't support "++" and "--"
 # i++
 
-#* ----------------- Logical -----------------
-#* and ("&&")
-#* or ("||")
-#* not ("!")
+# * ----------------- Logical -----------------
+# * and ("&&")
+# * or ("||")
+# * not ("!")
 
-#* ----------------- Others -----------------
+# * ----------------- Others -----------------
 
-#* "in" / "not in"
+# * "in" / "not in"
 print(3 in [1, 3, 6])
 print("us" in "sushi")
 print(2 not in (2, 7, 4))
-print("id" in {"id": 3}) #* Same as .keys()
+print("id" in {"id": 3})  # * Same as .keys()
 
-#! Don't use "eval" for production code
+# ! Don't use "eval" for production code
 # print(eval("1+2"))
