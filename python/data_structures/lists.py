49544547e105831bb7bac8e532758aeb2f60eff0
Pedro <pedro.saravia.q@uni.pe>
2024-12-26 18:53:19 -0500

    refactor lists

--- python/data_structures/lists.py
+++ python/data_structures/lists.py
@@ -1,60 +1,52 @@
+#
+# ^ You can use some operators with lists
+print(f'["a"] * 5: {["a"] * 5}')
+print(f"[1, 2] + [3, 4]: {[1, 2] + [3, 4]}")
+
 fruits = ["apple", "banana", "pear", "banana"]
-print("fruits:\t\t\t\t\t", fruits)
-
-#? Some curiosities of lists (strings, etc.) in Python
-
-#* In Python, negative indexes start from the end
-# print("fruits[-1]:", fruits[-1])
 # print(fruits[10]) #! You can't use indexes out of range
+print("\nfruits:\t\t\t\t\t", fruits)
 
-print(f'\n["a"] * 5: {["a"] * 5}')
-print(f'[1, 2] + [3, 4]: {[1, 2] + [3, 4]}')
-
-#?????????????????????????????????????????????????????
-
-#* "append"
+# * "append"
 fruits.append("lemon")
-print("\n.append(\"lemon\"):\t\t\t", fruits)
+print('\n.append("lemon"):\t\t\t', fruits)
 
-#* "insert" takes additionally an index
+# * "insert" takes additionally an index
 fruits.insert(3, "orange")
-print("\n.insert(3, \"orange\"):\t\t\t", fruits)
+print('\n.insert(3, "orange"):\t\t\t', fruits)
 
-#! You can use "del" to remove an element
+# ! You can use "del" to remove an element
 del fruits[0]
 print("\ndel fruits[0]:\t\t\t\t", fruits)
 
-#* "pop"
+# * "pop"
 eaten_fruit = fruits.pop()
 print("\n.pop():\t\t\t\t\t", fruits)
 # print(eaten_fruit)
-#! ...it can also take an index
-fruits.pop(1)
-print(".pop(1):\t\t\t\t", fruits)
+# ! ...it can also take an index
 
-#* "remove" deletes the first occurrence of a value
+# * "remove" deletes the first occurrence of a value
 fruits.remove("banana")
-print("\n.remove(\"banana\"):\t\t\t", fruits)
+print('\n.remove("banana"):\t\t\t', fruits)
 
-#* "sort" (has additionally a "reverse" argument)
+# * "sort"
+# ^ And it has also a "reverse" argument
 fruits.sort()
 print("\n.sort():\t\t\t\t", fruits)
-#! "sorted" doesn't change the original list
+# ~ "sorted" changes temporarily the list
 # print("sorted(fruits):", sorted(fruits))
 
-#* "reverse"
+# * "reverse"
 fruits.reverse()
 print("\n.reverse():\t\t\t\t", fruits)
 
-print("\n-------- Additional methods --------")
-
-#* "extend" adds multiple elements
+# * "extend" adds multiple elements
 fruits.extend(["strawberry", "mango"])
-print("\n.extend([\"strawberry\", \"mango\"]):\t", fruits)
+print('\n.extend(["strawberry", "mango"]):\t', fruits)
 
 numbers = [7, 4, 7, 7, 1]
 print("\nnumbers:", numbers)
-#* "count"
+# * "count"
 print(".count(7):", numbers.count(7))
-#* "index"
+# * "index"
 print(".index(1):", numbers.index(1))
