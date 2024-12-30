e0618c37c03b31d4219653e907867e3c1fcedbe6
Pedro <pedro.saravia.q@uni.pe>
2024-12-29 20:10:22 -0500

    refactor loops and tuples

--- python/loops.py
+++ python/loops.py
@@ -1,24 +1,24 @@
-
-#* "for" loops
+#
+# * "for" loops
 for letter in "LOOP":
     print(letter)
 
 for i in range(4):
-    print(f'int: {i}')
+    print(f"int: {i}")
 
-#* "while" loops
+# * "while" loops
 count = 3
 while count > 0:
     print(f"{count}...")
     count -= 1
 
-#* "continue" is used to skip an iteration
+# * "continue" is used to skip an iteration
 for i in range(10):
     if i % 2 == 0:
         continue
     print(i)
 
-#* "break" is used to exit the whole loop
+# * "break" is used to exit the whole loop
 animal = "fish"
 while True:
     if animal == "fish":
@@ -27,9 +27,9 @@ while True:
 
 print("------------ ADVANCED ------------")
 
-#* "enumerate" is used when you need the index and the element of a sequence
-for index, value in enumerate(['a', 'b', 'c']):
-    print(f'index: {index} , value: {value}')
-#* The "else" block is executed when the loop is completed without a "break"
+# * "enumerate" is used when you need the index and the element of a sequence
+for index, value in enumerate(["a", "b", "c"]):
+    print(f"index: {index} , value: {value}")
+# * The "else" block is executed when the loop is completed without a "break"
 else:
     print("The loop completed without a 'break'")
