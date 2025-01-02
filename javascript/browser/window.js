42c147b9f5156eb58081d07837d327acf1aa563d
Pedro <pedro.saravia.q@uni.pe>
2025-01-02 18:13:47 -0500

    add alert and prompt

---
+++ javascript/browser/window.js
@@ -0,0 +1,7 @@
+//* "window" is a global object in the browser
+//! It doesn't work in Node.js
+//~ Functions and properties don't need to be called on the window object
+
+//* alert / prompt
+// alert("Hello, World!");
+// let input = prompt("What's your name?");
