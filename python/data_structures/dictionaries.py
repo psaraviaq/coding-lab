7a50f71d3fc923dfc072a405d747ef6b3989c8d4
Pedro <pedro.saravia.q@uni.pe>
2025-01-02 12:18:33 -0500

    refactor sets and dictionaries

--- python/data_structures/dictionaries.py
+++ python/data_structures/dictionaries.py
@@ -1,36 +1,34 @@
-
 person = {"name": "Pedro", "age": 22, "color": "blue"}
 print(f"person:\t\t\t\t{person}")
 
-#*-------------- Access --------------
 print(f'\n["name"]: {person["name"]}')
 #! You can't use the dot notation
 # print(person.name)
-#* "get" is a safer way to access
+# * "get" is a safer way to access
 print(f'.get("degree"): {person.get("degree")}')
 # print(person.get("country", "Peru"))
 
-#* "del" removes a key-value pair
+# * "del" removes a key-value pair
 del person["color"]
 print(f'\ndel person["color"]:\t\t{person}')
 
-#*-------------- Loops ---------------
 print("\n.items(): key, value")
 for key, value in person.items():
-    print(f'\t{key}:\t{value}')
+    print(f"\t{key}:\t{value}")
 
 print("\n.keys()\t.values()")
-for key, value in person.keys(), person.values():
-    print(f'{key}\t{value}')
+for key, value in zip(person.keys(), person.values()):
+    print(f"{key}\t{value}")
 
-#*-------------- Others ---------------
+# * Copying
+print(f"\n.copy(): {person.copy()}")
+print(f"{{**person}}: {dict({**person})}")
 
-#* Dictionary comprehensions
-# print({str(x): x**3 for x in range(1, 6)})
-# months = ["Jan", "Feb", "Mar", "Apr"]
-# print({a+1: b for (a, b) in zip(range(10), months)})
+# * "update"
+# ^ You can also add new key-value pairs
+person.update({"age": 24, "id": "1234"})
+print(f"\n.update({{'age': 24, 'id': '1234'}}): {person}")
 
-#* Operator
-# someone = {**person}
-# someone["name"] = "Ava"
-# print(person["name"], "&", someone["name"])
+# * "clear"
+person.clear()
+print(f"\n.clear(): {person}")
