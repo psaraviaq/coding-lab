1aa976d4882f3b1239258a39c6258d63fba0d510
Pedro <pedro.saravia.q@uni.pe>
2025-01-05 18:55:35 -0500

    refactor functions

--- python/functions.py
+++ python/functions.py
@@ -1,32 +1,46 @@
-
-#* "def"
+#
+# * "def"
 def sum(x, y):
     return x + y
+
+
 print(sum(5, 3))
 
 #! Arguments are required
 # print(sum())
-#* Keyword arguments
+# * Keyword arguments
 print(sum(y=2, x=7))
 
-#* "None"
+
+# * When no return is specified, the function returns "None"
 def no_return():
     pass
+
+
 print(no_return())
 
-#* Default parameters
+
+# * Default parameters
 def multiply(x, y=1):
     return x * y
+
+
 print(multiply(5))
 
-#* "*args" (tuple)
+
+# * "*args" (tuple)
 def pizza(*toppings):
     for topping in toppings:
         print(f"- {topping}")
+
+
 pizza("pepperoni", "mushrooms", "onions")
 
-#* "**kwargs" (dictionary)
+
+# * "**kwargs" (dictionary)
 def user_data(**person):
     for key, value in person.items():
         print(f"{key}: {value}")
+
+
 user_data(name="Pedro", age=24)
