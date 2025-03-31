57e6217203c0664ea92953bee67e871c2c211f37
Pedro <pedro.saravia.q@uni.pe>
2025-03-30 21:19:41 -0500

    add information about files and folders

--- c#/First/Program.cs
+++ c#/First/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//* Solution --> Projects
+
+//^ The ".sln" file contains a list of all the projects in the solution
+
+//^ Each project has a ".csproj" file, which contains information
+//^ about the project in order to build it and run it
+
+//^ The "bin" folder contains the compiled code and executable files
+//^ The "obj" folder contains temporary files used to build the project
+
+//& "dotnet run": Use this command inside the project folder
+
+Console.WriteLine("Hello, World!");
