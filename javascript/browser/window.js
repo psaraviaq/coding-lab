4df4847f153b960ee87fae66bac56651c016c018
Pedro <psaravia.q@gmail.com>
2025-05-02 21:09:49 -0500

    add alert and prompt

--- javascript/browser/window.js
+++ javascript/browser/window.js
@@ -2,6 +2,9 @@
 //! It doesn't work in Node.js
 //~ Functions and properties don't need to be called on the window object
 
-//* alert / prompt
+//* "alert" displays a message
 // alert("Hello, World!");
-// let input = prompt("What's your name?");
+
+//* "prompt" asks for user input and returns a string
+//! If it's cancelled, it returns null
+// let name = prompt("What's your name?");
