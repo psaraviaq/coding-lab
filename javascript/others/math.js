5edc1d0beb81c2e6d6e67c84454cacb0101190a8
Pedro <psaravia.q@gmail.com>
2025-05-03 05:38:42 -0500

    add math object

--- javascript/others/math.js
+++ javascript/others/math.js
@@ -1,7 +1,10 @@
-//* floor / ceil / round
+//^ The "Math" object
+
+//* "random" returns a random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random());
+
+//* "floor" and "ceil" round down and up, respectively
 console.log(Math.floor(1.9));
 console.log(Math.ceil(3.1));
-console.log(Math.round(7.5));
-
-//* random (0 <= ... < 1)
-console.log(Math.random());
+//* "round" rounds to the nearest integer
+console.log(Math.round(7.4), Math.round(7.6));
