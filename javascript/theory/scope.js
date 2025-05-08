5fcb348f4cd24af877681b76d584a79354502c5f
Pedro <psaravia.q@gmail.com>
2025-05-07 22:37:07 -0500

    add scope

---
+++ javascript/theory/scope.js
@@ -0,0 +1,34 @@
+//^ There are 3 types of scope:
+
+//* 1. Global scope
+//~ They are accessible from anywhere
+let username = "John";
+function globalGreet() {
+  console.log(`Hello ${username}`);
+}
+globalGreet();
+console.log(`Bye ${username}`);
+
+//* 2. Function scope (local variables)
+//~ They are only accessible from inside the function
+//& Parameters are also local, they are initialized when the function is called
+function localGreet(question) {
+  let username = "Kevin";
+  console.log(`Hello ${username}, ${question}`);
+  //! If you don't declare a variable in a function, it will be global
+  // password = "123";
+}
+localGreet("how are you?");
+console.log(`Bye ${username}`);
+// console.log(password);
+
+//* 3. Block scope (code between {})
+//~ They are only accessible from inside the block
+if (true) {
+  let username = "Jane";
+  console.log(`Hello ${username}`);
+  //! Same goes for code blocks
+  // let password = "123";
+}
+console.log(`Bye ${username}`);
+// console.log(password);
