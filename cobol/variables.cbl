cbb023fd0fd559f02c32d5704db62578627d36e9
Pedro <psaravia.q@gmail.com>
2025-05-17 19:54:06 -0500

    add records

--- cobol/variables.cbl
+++ cobol/variables.cbl
@@ -2,12 +2,21 @@
        PROGRAM-ID. Variables.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-
+       
       ** Fields ARE used TO store a single VALUE
       *^ It starts WITH a level indicator, THEN the name, the TYPE
       *! The first level should always be 01 or 77
        01 EMPLOYEE PIC X(20).
        
+      ** Records are used to store multiple related fields
+       01 CUSTOMER.
+           02 CUSTOMER-NAME        PIC X(20).
+           02 CUSTOMER-PASSWORD    PIC X(20).
+           
        PROCEDURE DIVISION.
+      ** You can use "MOVE" to assign a value
            MOVE "John" TO EMPLOYEE.
            DISPLAY EMPLOYEE.
+           
+           MOVE "Pedro" TO CUSTOMER-NAME.
+           DISPLAY CUSTOMER-NAME.
