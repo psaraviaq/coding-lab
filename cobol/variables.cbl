dab8589088f4ec4b8a994c185cab93d0abf08b15
Pedro <psaravia.q@gmail.com>
2025-05-17 20:15:02 -0500

    add more about records

--- cobol/variables.cbl
+++ cobol/variables.cbl
@@ -3,16 +3,23 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        
-      ** Fields ARE used TO store a single VALUE
-      *^ It starts WITH a level indicator, THEN the name, the TYPE
+      ** Fields are used to store a single value
+      *^ It starts with a level, then the name, and the picture clause
       *! The first level should always be 01 or 77
        01 EMPLOYEE PIC X(20).
        
       ** Records are used to store multiple related fields
+      *! The record never has a picture clause
        01 CUSTOMER.
+      *! The subsequent levels should be greater than the record,
+      *! they can go from 02 to 49
            02 CUSTOMER-NAME        PIC X(20).
            02 CUSTOMER-PASSWORD    PIC X(20).
-           
+      *^ You can even create a record inside a record
+           02 CUSTOMER-ADDRESS.
+               03 CUSTOMER-CITY    PIC X(20).
+               03 CUSTOMER-STATE   PIC X(20).
+               
        PROCEDURE DIVISION.
       ** You can use "MOVE" to assign a value
            MOVE "John" TO EMPLOYEE.
@@ -20,3 +27,5 @@
            
            MOVE "Pedro" TO CUSTOMER-NAME.
            DISPLAY CUSTOMER-NAME.
+           MOVE "Lima" TO CUSTOMER-CITY.
+           DISPLAY CUSTOMER-CITY.
