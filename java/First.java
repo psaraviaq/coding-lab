fbb5600a1a71138b2e16ef4300308aa03660ef9f
Pedro <psaravia.q@gmail.com>
2026-04-04 13:39:05 -0500

    modify first java

--- java/First.java
+++ java/First.java
@@ -1,19 +1,18 @@
 //! The class name must be the same as the filename
 public class First {
 
-    // * The "main" method is the entry point of the program, and it must have the
-    // signature:
+    //* The "main" method is the entry point of the program
     public static void main(String[] args) {
 
-        // * "print" doesn't add a new line at the end
+        //* "print" doesn't add a new line at the end
         System.out.print("Oi. ");
 
-        // * But "println" does, so it's the most common one
+        //* But "println" does, so it's the most common one
         System.out.println("Hello, World!");
 
-        // ! You can only use one argument when printing
+        //! You can only use one argument when printing
         // System.out.println("I'm", "Peter");
-        // * But you can concatenate them
+        //* But you can concatenate them
         System.out.println("I'm" + " Peter");
     }
 
