4cd612433de277dab3f88b15eedd958c70fdaead
Pedro <psaravia.q@gmail.com>
2026-05-01 23:54:42 -0500

    modify builtins

--- python/built_ins.py
+++ python/built_ins.py
@@ -1,9 +1,8 @@
 #
 # * "min", "max", "sum"
-print("min([1, 2, 3]):", min([1, 2, 3]))
-print("max([1, 2, 3]):", max([1, 2, 3]))
+print("min(1, 2, 3):", min(1, 2, 3))
+print("max(1, 2, 3):", max(1, 2, 3))
 print("sum([1, 2, 3]):", sum([1, 2, 3]))
 
-# * "range"
-# print("list(range(5)):", list(range(5)))
+# * "range" (start, stop, step)
 print("\nlist(range(1, 10, 2)):", list(range(1, 10, 2)))
