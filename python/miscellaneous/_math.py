4dab2119d39466254aca8f18a44eb8c10e0a5aa9
Pedro <psaravia.q@gmail.com>
2026-05-01 23:15:16 -0500

    add math

---
+++ python/miscellaneous/_math.py
@@ -0,0 +1,10 @@
+import math
+
+# * factorial
+print("5! =", math.factorial(5))
+
+# * perm (permutation)
+print("P(5, 2) =", math.perm(5, 2))
+
+# * comb (combination)
+print("C(5, 2) =", math.comb(5, 2))
