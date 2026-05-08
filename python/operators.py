813e66abc63dcdc4684c3d2b935cb4a1c0f9cc43
Pedro <psaravia.q@gmail.com>
2026-05-08 07:59:12 -0500

    add todo is/is not

--- python/operators.py
+++ python/operators.py
@@ -1,9 +1,4 @@
 #
-# * Multiple assignment
-a, b, c = 1, 2, 3
-print(a, b, c)
-
-# * ----------------- Mathematical -----------------
 # * Addition "+"
 # * Subtraction "-"
 # * Multiplication "*"
@@ -14,12 +9,9 @@ print(a, b, c)
 # ! Python doesn't support "++" and "--"
 # i++
 
-# * ----------------- Logical -----------------
 # * and ("&&")
 # * or ("||")
 # * not ("!")
-
-# * ----------------- Others -----------------
 
 # * "in" / "not in" (checks if a value is in a sequence)
 print(3 in [1, 3, 6])
@@ -27,6 +19,8 @@ print("us" in "sushi")
 print(2 not in (2, 7, 4))
 # ~ It also works with keys in dictionaries
 print("id" in {"id": 3})
+
+# TODO "is" / "is not" comprueba si dos variables son el mismo objeto en memoria
 
 # ! Don't use "eval" for production code
 # print(eval("1+2"))
