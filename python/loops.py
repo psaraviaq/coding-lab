5d31d0da0a9a20253dcf7109802b8fedc0ab2701
Pedro <psaravia.q@gmail.com>
2026-05-09 02:18:56 -0500

    modify loops

--- python/loops.py
+++ python/loops.py
@@ -1,26 +1,12 @@
 #
 # * "for" loops
-for letter in "LOOP":
-    print(letter)
-
 for i in range(4):
-    print(f"int: {i}")
-
-# * "while" loops
-count = 3
-while count > 0:
-    print(f"{count}...")
-    count -= 1
-
-# * "continue" is used to skip an iteration
-for i in range(10):
-    if i % 2 == 0:
-        continue
     print(i)
 
-# * "break" is used to exit the whole loop
-animal = "fish"
-while True:
-    if animal == "fish":
-        break
-    print("Wrong animal")
+# * "while" loops
+count = 1
+while count <= 3:
+    print(f"{count}...")
+    count += 1
+
+# * "continue" / "break"
