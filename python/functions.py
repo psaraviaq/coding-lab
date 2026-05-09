b14a25f6c5dca7b83c2b360eb1509bc9428eb2b8
Pedro <psaravia.q@gmail.com>
2026-05-09 10:42:15 -0500

    modify functions

--- python/functions.py
+++ python/functions.py
@@ -1,53 +1,13 @@
 #
-# * "def"
-def sum(x, y):
-    return x + y
+# * Define a function with the "def" keyword
+# ~ "None" is returned by default if there is no return statement
+def greet(name):
+    print(f"Hello, {name}!")
 
 
-print(sum(5, 3))
+greet("Alice")
+#! Arguments are mandatory unless you provide a default value
+# greet()
 
-# ! Arguments are required
-# print(sum())
-# * Keyword arguments
-print(sum(y=2, x=7))
-
-
-# * Default parameters
-def multiply(x, y=1):
-    return x * y
-
-
-print(multiply(5))
-
-
-# * You can return multiple values (tuple)
-def multiple_values():
-    return 1, 2, 3
-
-
-a, b, c = multiple_values()
-print(a, b, c)
-
-
-# * When no return is specified, the function returns "None"
-def no_return():
-    pass
-
-
-print(no_return())
-
-
-# * "*args" (tuple)
-def pizza(*toppings):
-    print(toppings)
-
-
-pizza("pepperoni", "mushrooms", "onions")
-
-
-# * "**kwargs" (dictionary)
-def user_data(**person):
-    print(person)
-
-
-user_data(name="Pedro", age=24)
+# * Named parameters allow you to specify arguments by name
+print("Hello", "World", sep=", ", end="!")
