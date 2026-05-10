3f46e9a16e48f8d8b66f5c147ba9a0228b560ddc
Pedro <psaravia.q@gmail.com>
2026-05-09 22:40:09 -0500

    move exceptions

--- python/oop/exceptions.py
+++ python/oop/exceptions.py
@@ -1,9 +1,24 @@
-#
-# * try / except
+import time, sys
+
+# * "try" / "except" blocks
+
 try:
-    age = int("24 years")
-    print(age)
-# except:
-#     print("Something went wrong.")
-except ValueError as e:
-    print("Error:", e)
+    while True:
+        print(".")
+        time.sleep(1)
+except KeyboardInterrupt:
+    sys.exit()
+
+
+# * "raise" allows you to throw an exception
+def area(length, width):
+    if length < 0 or width < 0:
+        raise ValueError("Length and width must be non-negative.")
+    return length * width
+
+
+try:
+    print(area(5, 3))
+    print(area(-1, 2))
+except ValueError:
+    print("An error occurred while calculating the area.")
